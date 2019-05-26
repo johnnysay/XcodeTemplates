f6576c327cc87fc2c8f08fc804f78a225f21a2b0
Johnny Saygnachith <johnny.saygnachith@gmail.com>
2019-05-27 00:23:38 +0200

    feat: use module as struct and add navigator pattern

--- MVP.xctemplate/___FILEBASENAME___Module.swift
+++ MVP.xctemplate/___FILEBASENAME___Module.swift
@@ -1,18 +1,22 @@
 import Foundation
 
-enum ___VARIABLE_featureName___Module {
+struct ___VARIABLE_featureName___Module {
 
     typealias Model = ___VARIABLE_featureName___ModelProtocol
     typealias View = ___VARIABLE_featureName___ViewProtocol
     typealias Presenter = ___VARIABLE_featureName___PresenterProtocol
     typealias Service = ___VARIABLE_featureName___ServiceProtocol
+    typealias Navigator = ___VARIABLE_featureName___Navigator
 
-    static var viewController: ___VARIABLE_featureName___ViewController {
-        let vc = ___VARIABLE_featureName___ViewController.instantiate()
+    let viewController: ___VARIABLE_featureName___ViewController
+
+    init(navigator: ___VARIABLE_featureName___Module.Navigator,
+         configuration: ___VARIABLE_featureName___ModuleConfiguration) {
+        let viewController = ___VARIABLE_featureName___ViewController.instantiate()
         let model = ___VARIABLE_featureName___Model()
-        let presenter = ___VARIABLE_featureName___Presenter(model: model, view: vc)
-        vc.presenter = presenter
-        return vc
+        let presenter = ___VARIABLE_featureName___Presenter(model: model, view: viewController)
+        viewController.presenter = presenter
+        self.viewController = viewController
     }
 }
 
